4eb2b8cb6810397f8f27b5063b5d2290c9bf7d78
Stefan <96178532+stefan6419846@users.noreply.github.com>
2026-09-13 14:56:12 +0200

    Update to latest V8 version of Chromium 139

--- settings.py
+++ settings.py
@@ -8,10 +8,9 @@ DEPOT_HOME = os.environ.get("DEPOT_HOME", os.path.join(STPYV8_HOME, "depot_tools
 V8_HOME = os.environ.get("V8_HOME", os.path.join(STPYV8_HOME, "v8"))
 
 V8_GIT_URL = "https://chromium.googlesource.com/v8/v8.git"
-# 13.9.210
 # 14.9.209
 # 15.5.31
-V8_GIT_TAG_STABLE = "13.1.201.22"
+V8_GIT_TAG_STABLE = "13.9.210"
 V8_GIT_TAG_MASTER = "master"
 V8_GIT_TAG = V8_GIT_TAG_STABLE
 DEPOT_GIT_URL = "https://chromium.googlesource.com/chromium/tools/depot_tools.git"
